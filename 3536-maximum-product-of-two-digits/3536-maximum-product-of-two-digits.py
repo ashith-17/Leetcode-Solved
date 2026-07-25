72e36a4c04ea7fe452cdc258b429062031275e7b
Ashith Vinay Raj <167337806+ashith-17@users.noreply.github.com>
2026-07-25 23:58:38 +0530

    Time: 0 ms (100%), Space: 19.1 MB (88.69%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, n: int) -> int:
+        
+        largest = 0
+        second = 0
+
+        while n:
+            digit = n % 10
+            if digit >= largest:
+                second = largest
+                largest = digit
+            elif digit > second:
+                second = digit
+            n //= 10
+        return largest * second
