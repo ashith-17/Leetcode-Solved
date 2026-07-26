2492d3d0ca0408bad8dc9ba2cbe6a04074407b29
Ashith Vinay Raj <167337806+ashith-17@users.noreply.github.com>
2026-07-26 13:17:58 +0530

    Update 0550-game-play-analysis-iv.sql

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,14 +1,16 @@
-SELECT ROUND(
-   COUNT(a.player_id) /
+SELECT
+    ROUND(
+        COUNT(*) /
         (SELECT COUNT(DISTINCT player_id) FROM Activity),
         2
     ) AS fraction
-FROM 
-(
-    SELECT player_id,MIN(event_date) AS first_date
+FROM Activity a
+JOIN (
+    SELECT
+        player_id,
+        MIN(event_date) AS first_date
     FROM Activity
     GROUP BY player_id
-)first_login 
-JOIN Activity a
-ON a.player_id=first_login.player_id
-AND a.event_date = DATE_ADD(first_login.first_date, INTERVAL 1 DAY);
+) f
+ON a.player_id = f.player_id
+AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY);
