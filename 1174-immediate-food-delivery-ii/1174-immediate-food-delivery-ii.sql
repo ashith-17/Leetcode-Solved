da261310b69a0dd485f2e7eb8d90da5fb117411d
Ashith Vinay Raj <167337806+ashith-17@users.noreply.github.com>
2026-07-26 11:08:25 +0530

    Time: 659 ms (51.87%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,22 @@
+SELECT
+    ROUND(
+        AVG(
+            CASE
+                WHEN d.order_date = d.customer_pref_delivery_date
+                THEN 1
+                ELSE 0
+            END
+        ) * 100,
+        2
+    ) AS immediate_percentage
+FROM Delivery d
+JOIN
+(
+    SELECT
+        customer_id,
+        MIN(order_date) AS first_order
+    FROM Delivery
+    GROUP BY customer_id
+) first_orders
+ON d.customer_id = first_orders.customer_id
+AND d.order_date = first_orders.first_order;
