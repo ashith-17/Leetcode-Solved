eb593cc5fbae4725739e06138fb73818caf7ec36
Ashith Vinay Raj <167337806+ashith-17@users.noreply.github.com>
2026-07-28 12:58:32 +0530

    Time: 821 ms (11.63%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,23 +1,28 @@
 SELECT
     p.product_id,
-    COALESCE(last_price.new_price, 10) AS price
+    COALESCE(pr.new_price, 10) AS price
 FROM
 (
     SELECT DISTINCT product_id
     FROM Products
 ) p
+
 LEFT JOIN
 (
-    SELECT product_id, new_price
-    FROM Products
-    WHERE (product_id, change_date) IN
+    SELECT p1.product_id,
+           p1.new_price
+    FROM Products p1
+    JOIN
     (
         SELECT
             product_id,
-            MAX(change_date)
+            MAX(change_date) AS latest_date
         FROM Products
         WHERE change_date <= '2019-08-16'
         GROUP BY product_id
-    )
-) last_price
-ON p.product_id = last_price.product_id;
+    ) p2
+    ON p1.product_id = p2.product_id
+    AND p1.change_date = p2.latest_date
+) pr
+
+ON p.product_id = pr.product_id;
